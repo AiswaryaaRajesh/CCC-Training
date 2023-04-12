0f3ccf795126fb662dd1e36b530f45e2ea2d6d1b
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-12 10:21:49 +0530

    check prime

--- Phase 2/12/12-04-23.c
+++ Phase 2/12/12-04-23.c
@@ -12,5 +12,33 @@ for(i=1,j=10;i<10,j>8;i++,j--)
 }                             #excute 2 times as j>8
 
   
-#switch case.....if it does not encounter a break statement, it will keep on executing statements Eg. if ch=2, it will execute casse 2 and the next cases 
+#switch case.....if it does not encounter a break statement, it will keep on executing statements Eg. if ch=2, it will execute case 2 and the next cases 
 # till it encounters break or switch casse ends
+  
+#check if prime or not....n between 1 and 10^5
+int n,i;
+scanf("%d",&n);
+if(n==1)
+{
+    printf("No")
+}
+else if (n==2 || n==3)
+{
+    printf("Yes")
+}
+else if (n%2==0 || n%3==0)
+{
+    printf("No")
+}
+else
+{
+    for(i=5;i*i<=n;i+=6)
+    {
+        if (n%i==0 || n%(i+2)==0)
+        {
+             printf("No");
+             exit(0);
+        }
+    }
+    printf("Yes")
+}
