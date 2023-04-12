d1dca0fc5996034806bd6688e7562abb9cc4568d
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-12 09:45:09 +0530

    Create 12-04-23.c

---
+++ Phase 2/12/12-04-23.c
@@ -0,0 +1,16 @@
+#infinite loop
+for(i=1;i=5,i<10;i++)
+{
+  printf("do i ever stop?")
+}                             #infinite execution
+
+
+#multiple conditions...only rigth most condition is important
+for(i=1,j=10;i<10,j>8;i++,j--)
+{
+  printf("Good Day!");
+}                             #excute 2 times as j>8
+
+  
+#switch case.....if it does not encounter a break statement, it will keep on executing statements Eg. if ch=2, it will execute casse 2 and the next cases 
+# till it encounters break or switch casse ends
